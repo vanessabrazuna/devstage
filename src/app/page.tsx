6387fdbdaf5707d8b4c4c307b5db99483b9d30a2
vanessabrazuna <brazuna.nessa@gmail.com>
2025-03-05 20:13:56 -0300

    feat: Add input patterns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { ArrowRight, Copy } from "lucide-react"
+import { ArrowRight, Copy, Mail } from "lucide-react"
+
 import { Button } from "@/components/button"
 import { IconButton } from "@/components/icon-button"
+import { InputField, InputIcon, InputRoot } from "@/components/input"
 
 export default function Home() {
   return (
@@ -13,6 +15,16 @@ export default function Home() {
       <IconButton>
         <Copy />
       </IconButton>
+
+      <div>
+        <InputRoot>
+          <InputIcon>
+            <Mail className="size-5" />
+          </InputIcon>
+
+          <InputField />
+        </InputRoot>
+      </div>
     </main>
   )
 }
