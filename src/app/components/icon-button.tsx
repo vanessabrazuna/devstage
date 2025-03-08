0ccf9bc75c21d504b7001e07ff1b65a05e6d555b
vanessabrazuna <brazuna.nessa@gmail.com>
2025-03-07 21:04:15 -0300

    fix: Update Settings Prettier

--- src/app/components/icon-button.tsx
+++ src/app/components/icon-button.tsx
@@ -8,7 +8,7 @@ export function IconButton({ className, ...props }: IconButtonProps) {
     <button
       className={twMerge(
         'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300',
-        className,
+        className
       )}
       {...props}
     />
